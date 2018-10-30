522db927edf4ed17613e721f352b4c4b8624af35
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-30 14:15:34 -0700

    updated for freemarker

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
@@ -1,14 +1,14 @@
 package edu.ucsb.cs56.pconrad.springboot.hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 
 }
